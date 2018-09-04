7d384b74f4d0f23ba3515a35d2a2b3531b3595fd
krizsipeti <krizsipeti@freemail.hu>
2018-09-04 15:19:27 +0200

    Create Register Functionality

---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 100, ErrorMessage = "Password must be at least 8 characters long and maximum 100.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password not match with confirmation.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
